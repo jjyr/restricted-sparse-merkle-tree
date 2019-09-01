fb9e067cd164a3ed80e77d49bc3f2b2d6821717a
jjy <jjyruby@gmail.com>
2019-09-01 21:25:19 +0800

    make blake2b module public

--- src/blake2b.rs
+++ src/blake2b.rs
@@ -1,8 +1,8 @@
 use blake2b_rs::{Blake2b, Blake2bBuilder};
 
-const BLAKE2B_KEY: &[u8] = &[];
-const BLAKE2B_LEN: usize = 32;
-const PERSONALIZATION: &[u8] = b"sparsemerkletree";
+pub const BLAKE2B_KEY: &[u8] = &[];
+pub const BLAKE2B_LEN: usize = 32;
+pub const PERSONALIZATION: &[u8] = b"sparsemerkletree";
 
 pub fn new_blake2b() -> Blake2b {
     Blake2bBuilder::new(BLAKE2B_LEN)
