188ee65862f6cb4f25b056a7f81d397857df8c80
jjy <jjyruby@gmail.com>
2020-02-18 15:20:19 +0800

    remove failure

--- src/error.rs
+++ src/error.rs
@@ -1,17 +1,29 @@
 use crate::H256;
-use failure::Fail;
 
-#[derive(Debug, Fail, Clone, PartialEq)]
+pub type Result<T> = ::std::result::Result<T, Error>;
+
+#[derive(Debug, Clone, PartialEq)]
 pub enum Error {
-    #[fail(
-        display = "Missing key, store maybe corrupt: height {} key {:?}",
-        _0, _1
-    )]
     MissingKey(usize, H256),
-    #[fail(display = "Corrupted proof")]
     CorruptedProof,
-    #[fail(display = "Empty proof")]
     EmptyProof,
 }
 
-pub type Result<T> = ::std::result::Result<T, Error>;
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::MissingKey(height, key) => {
+                write!(f, "Missing key at height {}, key {:?}", height, key)?;
+            }
+            Error::CorruptedProof => {
+                write!(f, "Corrupted proof")?;
+            }
+            Error::EmptyProof => {
+                write!(f, "Empty proof")?;
+            }
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for Error {}
